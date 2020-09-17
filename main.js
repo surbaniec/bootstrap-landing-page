8529e69a4ccebeeab887d8876f5056a2f30bcb4b
surbaniec <radsler1999@gmail.com>
2020-09-17 13:01:04 +0200

    mobile view done, add form validation, add hover states

--- main.js
+++ main.js
@@ -38,3 +38,20 @@ features.forEach(feature => {
         }
     })
 })
+
+const form = document.querySelector('.contact__form');
+const input = document.querySelector('.contact__input');
+const messageDiv = document.querySelector('.contact__form-message');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const reg = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$/;
+
+    if (!reg.test(input.value)) {
+        messageDiv.style.display = 'block';
+        input.style.border = '2px solid hsl(0, 94%, 66%)';
+    }
+    else {
+        e.target.submit();
+    }
+})
